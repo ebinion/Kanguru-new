ece9d9d1965cbb2e6f06a0c35a5b761962c58ad7
Ezekiel Binion <ebinion@gmail.com>
2012-06-14 16:45:32 -0500

    Added viewport info to application_layout

--- app/helpers/current_recommendations_helper.rb
+++ app/helpers/current_recommendations_helper.rb
@@ -1,15 +1,14 @@
 module CurrentRecommendationsHelper
   
   def condition_class(current_weather)
-    if self.current_weather.match('rain')
-      
-      # "rain"
-      #     
-      #     
-      # "sunny"
-      #     
-      # "snow"
     
+    if current_weather.condition.downcase.match('rain') || current_weather.condition.downcase.match('drizzle')
+      return "rain"
+    elsif current_weather.condition.downcase.match('sunny') || current_weather.condition.downcase.match('cloud')
+      return "sunny"
+    elsif current_weather.condition.downcase.match('snow')
+      return "snow"
     end
+    
   end
 end
