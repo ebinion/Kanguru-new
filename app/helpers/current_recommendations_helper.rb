8c29a6381249bf645fd370590bd1c5e5b2f3fdcc
Ezekiel Binion <ebinion@gmail.com>
2012-06-14 19:07:33 -0500

    Something with the helper

--- app/helpers/current_recommendations_helper.rb
+++ app/helpers/current_recommendations_helper.rb
@@ -2,7 +2,7 @@ module CurrentRecommendationsHelper
   
   def condition_class(current_weather)
     
-    if current_weather.condition.downcase.match('rain') || current_weather.condition.downcase.match('drizzle')
+    if current_weather.condition.downcase.match('rain') || current_weather.condition.downcase.match('drizzle')  || current_weather.condition.downcase.match('mist') 
       return "rain"
     elsif current_weather.condition.downcase.match('sunny') || current_weather.condition.downcase.match('cloud')
       return "sunny"
