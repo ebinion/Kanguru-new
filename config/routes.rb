134db8cf7909afb418e9828866d20a73912f6307
Ezekiel Binion <ebinion@gmail.com>
2012-06-18 18:00:17 -0500

    Slightly responsive homepage and working on the weather page'

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ KanguruApp::Application.routes.draw do
   
   get "recommendation" => "CurrentRecommendations#index", :as => "recommendation"
   post "recommendation" => "CurrentRecommendations#index", :as => "recommendation"
+  
+  get "test" => "CurrentRecommendations#test"
 end
